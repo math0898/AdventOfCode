927e7c650e24895e8f226ae811fd2d86e2fe37a6
math0898 <25396616+math0898@users.noreply.github.com>
2021-11-30 23:52:54 -0600

    Day 1 - Prob 1
    This is the state of code when I successfully completed the first problem of day 1. I will clean it up later.

---
+++ Day1/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea
+     * floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the
+     * sea floor depth as the sweep looks further and further away from the submarine.
+     *
+     * The first order of business is to figure out how quickly the depth increases, just so you know what you're
+     * dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or
+     * something.
+     *
+     * To do this, count the number of times a depth measurement increases from the previous measurement. (There is no
+     * measurement before the first measurement.)
+     *
+     * @param args The arguments given to the executable.
+     */
+    public static void main (String args[]) {
+        String filepath = "Day1/input.txt";
+        int count = 0;
+        try {
+            File f = new File(filepath);
+            System.out.println(f.getAbsoluteFile());
+            Scanner s = new Scanner(f);
+            int previous = 0;
+            int current = Integer.parseInt(s.nextLine());
+            while (s.hasNextLine()) {
+                previous = current;
+                current = Integer.parseInt(s.nextLine());
+                if (current > previous) count++;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
+        }
+        System.out.println("The depth increased: " + count + " times.");
+    }
+}
