45cf6c0181432be105b4aecb9b390465cdb54bdd
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-01 23:49:58 -0600

    Day 2
    Finished with day 2 decided to add the full description to each section.

--- Day2/src/Main.java
+++ Day2/src/Main.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,21 +9,75 @@ public class Main {
     private static final String filename = "Day2/input.txt";
 
     /**
+     * It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
      *
+     * - `forward X` increases the horizontal position by X units.
+     * - `down X` increases the depth by X units.
+     * - `up X` decreases the depth by X units.
      *
-     * @param data The data to use whilst counting.
+     * The submarine seems to already have a planned course (your puzzle input).
+     *
+     * Calculate the horizontal position and depth you would have after following the planned course. What do you get if
+     * you multiply your final horizontal position by your final depth?
      */
-    public static void part1 (ArrayList<Integer> data) {
-
+    public static void part1 () {
+        try {
+            File f = new File(filename);
+            Scanner s = new Scanner(f);
+            int horizontal = 0;
+            int depth = 0;
+            while (s.hasNextLine()) {
+                String input = s.next();
+                switch (input) {
+                    case "forward" -> horizontal += s.nextInt();
+                    case "up" -> depth -= s.nextInt();
+                    case "down"-> depth += s.nextInt();
+                }
+            }
+            System.out.println("Part 1 (Horizontal, Depth): (" + horizontal + ", " + depth + ") => " + horizontal * depth);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
+        }
     }
 
     /**
+     * In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at
+     * 0. The commands also mean something entirely different than you first thought:
      *
+     * - `down X` increases your aim by X units.
+     * - `up X` decreases your aim by X units.
+     * - `forward X` does two things:
+     *      - It increases your horizontal position by X units.
+     *      - It increases your depth by your aim multiplied by X.
      *
-     * @param data The data to use whilst counting.
+     * Using this new interpretation of the commands, calculate the horizontal position and depth you would have after
+     * following the planned course. What do you get if you multiply your final horizontal position by your final depth?
      */
-    public static void part2 (ArrayList<Integer> data) {
-
+    public static void part2 () {
+        try {
+            File f = new File(filename);
+            Scanner s = new Scanner(f);
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+            while (s.hasNextLine()) {
+                String input = s.next();
+                switch (input) {
+                    case "forward" -> {
+                        int t = s.nextInt();
+                        horizontal += t;
+                        depth += t * aim;
+                    }
+                    case "up" -> aim -= s.nextInt();
+                    case "down"-> aim += s.nextInt();
+                }
+            }
+            System.out.println("Part 2 (Horizontal, Depth): (" + horizontal + ", " + depth + ") => " + horizontal * depth);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
+        }
     }
 
     /**
@@ -33,24 +86,7 @@ public class Main {
      * @param args The arguments given to the executable.
      */
     public static void main (String[] args) {
-        try {
-            File f = new File(filename);
-            Scanner s = new Scanner(f);
-            int horizontal = 0;
-            int depth = 0;
-            int aim = 0;
-            while (s.hasNextLine()) {
-                String input = s.nextLine();
-                if (input.contains("forward")) {
-                    horizontal += Integer.parseInt(input.replace("forward ", ""));
-                    depth += aim * Integer.parseInt(input.replace("forward ", ""));
-                } else if (input.contains("up")) aim -= Integer.parseInt(input.replace("up ", ""));
-                else if (input.contains("down")) aim += Integer.parseInt(input.replace("down ", ""));
-            }
-            System.out.println(horizontal * depth);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
-        }
+        part1();
+        part2();
     }
 }
