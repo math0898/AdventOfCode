f3a31a7588b766dd4dc7d0a64d8e9e0b8c574560
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-01 22:17:08 -0600

    Cleaned
    Tweaked and clean Day 1 in preperation for day 2's release. I'm going to try and make the leaderboards. If I don't today I'll start swaping languages.

--- Day2/src/Main.java
+++ Day2/src/Main.java
@@ -3,19 +3,43 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    /**
+     * The relative path, starting from project source, to the input file.
+     */
+    private static final String filename = "Day1/input.txt";
+
     /**
      *
      *
+     * @param data The data to use whilst counting.
+     */
+    public static void part1 (ArrayList<Integer> data) {
+
+    }
+
+    /**
+     *
+     *
+     * @param data The data to use whilst counting.
+     */
+    public static void part2 (ArrayList<Integer> data) {
+
+    }
+
+    /**
+     * The main execution point of the program.
+     *
      * @param args The arguments given to the executable.
      */
-    public static void main (String args[]) {
-        String filepath = "Day2/input.txt";
-        int count = 0;
+    public static void main (String[] args) {
         try {
-            File f = new File(filepath);
+            File f = new File(filename);
             Scanner s = new Scanner(f);
             ArrayList<Integer> data = new ArrayList<>();
             while (s.hasNextLine()) data.add(Integer.parseInt(s.nextLine()));
+            part1(data);
+            part2(data);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
