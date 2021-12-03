6331304f66129c3effeab11594eb587b3eb14255
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-02 23:32:37 -0600

    Part 1

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -1,13 +1,50 @@
 #include <fstream>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-int main () {
+/**
+ * Converts the given string which represents a binary number into the corresponding decimal value. 
+ * 
+ * @param s The string to convert.
+ * @return The decimal value of the given string.
+ */
+int convertBinary (string s) {
+    int r = 0;
+    for (int i = 0; i < s.size(); i++) {
+        r *= 2;
+        if (s[i] == '1') r++;
+    }
+    return r;
+}
+
+int main (int argc, char** argv) {
     ifstream* file = new ifstream();
-    file->open("example.txt");
+    file->open("input.txt");
 
     if (!file->is_open()) return -1; // Failed to open file.
 
-    cout << "Hello world" << endl;
+    string input;
+    *file >> input;
+
+    int* gammaCounts = new int[input.size()]{0};
+    int elapsed = 1;
+    for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
+
+    while (!file->eof()) {
+        *file >> input;
+        elapsed++;
+        for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
+    }
+
+    string temp = "";
+    for (int i = 0; i < input.size(); i++) {
+        if (gammaCounts[i] > (elapsed/2)) temp += '1';
+        else temp += '0';
+    }
+
+    unsigned int gamma = convertBinary(temp);
+
+    cout << "The Power Consumption is: " << gamma * (~gamma % (int) pow(2, input.size())) << endl;
 }
