824977fc1849d7e9224b6b55a0ac98d84abcbd09
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-03 01:05:14 -0600

    Description
    I've added the description and started cleaning code... still a lot to do.

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 /**
  * Inserts the given item into the int array. Not ideal since it has to resize the int array nearly every time.
- * 
+ *
  * The story behind this is I wanted a dynamically allocated structure but my friend would denounce me if I used vector.
- * 
+ *
  * @param array The int array to resize.
  * @param i The int to add into the array.
  * @param n The length of the array.
  */
-void resize (int** array, int i, int n) {
-    int* temp = new int[n + 1];
+void resize (int **array, int i, int n) {
+    int *temp = new int[n + 1];
     for (int j = 0; j < n; j++) temp[j] = (*array)[j];
     temp[n] = i;
-    delete(*array);
+    delete (*array);
     *array = temp;
 }
 
 /**
- * Converts the given string which represents a binary number into the corresponding decimal value. 
- * 
+ * Converts the given string which represents a binary number into the corresponding decimal value.
+ *
  * @param s The string to convert.
  * @return The decimal value of the given string.
  */
@@ -36,40 +36,51 @@ int convertBinary (string s) {
     return r;
 }
 
-int main (int argc, char** argv) {
-    ifstream* file = new ifstream();
-    file->open("input.txt");
+/**
+ * You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the
+ * epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate
+ *
+ * Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in
+ * the diagnostic report.
+ *
+ * The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position
+ * is used.
+ *
+ * @param filename The file to open and read from to get input.
+ */
+void part1 (string filename) {
+    ifstream *file = new ifstream();
+    file->open(filename);
+    if (!file->is_open()) return; // Failed to open file.
+    string input;
+    *file >> input;
+    int *gammaCounts = new int[input.size()]{0};
+    int elapsed = 1;
+    for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
+    while (!file->eof()) {
+        *file >> input;
+        elapsed++;
+        for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
+    }
+    string temp = "";
+    for (int i = 0; i < input.size(); i++) { gammaCounts[i] > (elapsed / 2) ? temp += '1' : temp += '0'; }
+    unsigned int gamma = convertBinary(temp);
+    cout << "The Power Consumption is: " << gamma * (~gamma % (int)pow(2, input.size())) << endl;
+}
 
-    if (!file->is_open()) return -1; // Failed to open file.
-
-    // part 1
-    // string input;
-    // *file >> input;
-
-    // int* gammaCounts = new int[input.size()]{0};
-    // int elapsed = 1;
-    // for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
-
-    // while (!file->eof()) {
-    //     *file >> input;
-    //     elapsed++;
-    //     for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
-    // }
-
-    // string temp = "";
-    // for (int i = 0; i < input.size(); i++) {
-    //     if (gammaCounts[i] > (elapsed/2)) temp += '1';
-    //     else temp += '0';
-    // }
-
-    // unsigned int gamma = convertBinary(temp);
-
-    // cout << "The Power Consumption is: " << gamma * (~gamma % (int) pow(2, input.size())) << endl;
-
+/**
+ * Part 2 has quite the lengthy definition even for the important information. You can find it in `description.md`.
+ *
+ * @parm filename The file to open and read from to get input.
+ */
+void part2 (string filename) {
+    ifstream *file = new ifstream();
+    file->open(filename);
+    if (!file->is_open()) return; // Failed to open file.
     string input;
     *file >> input;
     int length = input.size();
-    int* list = new int[1]{0};
+    int *list = new int[1]{0};
     int size = 1;
     list[0] = convertBinary(input);
     while (!file->eof()) {
@@ -77,7 +88,6 @@ int main (int argc, char** argv) {
         resize(&list, convertBinary(input), size);
         size++;
     }
-
     int remaining = size;
     for (int i = 1; i <= length; i++) {
         if (remaining == 1) break;
@@ -88,8 +98,7 @@ int main (int argc, char** argv) {
             if ((list[j] & power) == power) onesCount++;
         }
         int require = 0;
-        if (onesCount >= (remaining/2.0)) require = power;
-        cout << onesCount << "/" << remaining/2 << " Ones => " << require << endl;
+        if (onesCount >= (remaining / 2.0)) require = power;
         for (int j = 0; j < size; j++) {
             if (list[j] == -1) continue;
             if ((list[j] & power) != require) {
@@ -109,7 +118,6 @@ int main (int argc, char** argv) {
         }
     }
     cout << "Oxygen Generator Rating: " << oxygen << endl;
-
     list = new int[1]{0};
     size = 1;
     file->clear();
@@ -121,7 +129,6 @@ int main (int argc, char** argv) {
         resize(&list, convertBinary(input), size);
         size++;
     }
-
     remaining = size;
     for (int i = 1; i <= length; i++) {
         if (remaining == 1) break;
@@ -132,8 +139,7 @@ int main (int argc, char** argv) {
             if ((list[j] & power) == power) onesCount++;
         }
         int require = 0;
-        if (onesCount < (remaining/2.0)) require = power;
-        cout << onesCount << "/" << remaining/2.0 << " Ones => " << require << endl;
+        if (onesCount < (remaining / 2.0)) require = power;
         for (int j = 0; j < size; j++) {
             if (list[j] == -1) continue;
             if ((list[j] & power) != require) {
@@ -150,6 +156,14 @@ int main (int argc, char** argv) {
         }
     }
     cout << "CO2 Scrubber Rating: " << carbon << endl;
-
     cout << "Life Support Rating: " << carbon * oxygen << endl;
+}
+
+/**
+ * The main execution point of the program.
+ */
+int main () {
+    string file = "input.txt";
+    part1(file);
+    part2(file);
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent Of Code 2021
+
+This is my code for the 2021 Advent Of Code. I've included commits from when I first get the solution and then clean up the code afterwards to optimize things and improve readability. The three primary languages I will be moving between to keep things fresh are Java, C++, and C. Generally I'm most proficient in Java however since the global leaderboard is soo competitive I might as well have some fun with other languages.
