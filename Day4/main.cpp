4e62b39f487544b25258dbf1a93484e1f10d50eb
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-04 00:09:55 -0600

    Day 4

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <list>
+#include <string.h>
 
 using namespace std;
 
 int main (int argc, char** argv) {
-
     string filename = "example.txt";
-
     if (argc >= 2) filename = argv[1];
-
     ifstream* file = new ifstream();
     if (file->is_open()) return -1;
     file->open(filename);
+    list<int>* numbers = new list<int>();
+    char* inputs = new char[1000]{'a'};
+    file->getline(inputs, 1000, '\n');
+    char* read = new char[10]{'0'};
+    read = strtok(inputs, ",");
+    while (read != NULL) {
+        numbers->push_back(atoi(read));
+        read = strtok(inputs, ",");
+    }
+
+    while (numbers->size() > 0) {
+        cout << (int) numbers->front() << endl;
+        numbers->pop_front();
+    }
 
     return 0;
 }
