f4a39b1ada2a10da10aac83e8aefc2c453c23aea
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-03 22:57:08 -0600

    Quick

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -3,11 +3,15 @@
 
 using namespace std;
 
-int main () {
+int main (int argc, char** argv) {
+
+    string filename = "example.txt";
+
+    if (argc >= 2) filename = argv[1];
 
     ifstream* file = new ifstream();
     if (file->is_open()) return -1;
-    file->open("example.txt");
+    file->open(filename);
 
     return 0;
 }
