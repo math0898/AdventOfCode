bc15056478d87a18a90f193db9a1347d1f49b2c5
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-06 00:32:21 -0600

    Day 6 Part 2
    Hahahaha Linux long vs Windows long GG. I was trying to take a math approach but the developer approach was easier.

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -29,51 +29,50 @@ int getInt (char delim, int i, string s) {
 }
 
 /**
- * Inserts the given item into the int array. Not ideal since it has to resize the int array nearly every time. Copied from 
- * Day 5.
- *
- * The story behind this is I wanted a dynamically allocated structure but my friend would denounce me if I used vector.
- *
- * @param array The int array to resize.
- * @param i The int to add into the array.
- * @param n The length of the array.
+ * Shifts the members of the array.
+ * 
+ * @param array The array to shift each member.
+ * @param n The size of the array.
  */
 template <class T>
-void resize (T** array, T i, int n) {
-    T* temp = new T[n + 1];
-    for (int j = 0; j < n; j++) temp[j] = (*array)[j];
-    temp[n] = i;
+void shift (T** array, int n) {
+    T* temp = new T[n]{0};
+    for (int j = 0; j < n; j++) temp[(j - 1 + n) % n] = (*array)[j];
     delete (*array);
     *array = temp;
 }
 
 int main () {
-    int length = 1;
-    Lanternfish* fish = new Lanternfish[1];
+    long* fish = new long[9]{0};
     fstream* file = new fstream();
     file->open("input.txt");
     if (!file->is_open()) return -1; // Failed to open file.
     string input;
     *file >> input;
-    fish[0] = Lanternfish(input[0] - '0');
-    for (int i = 1; i < (input.size() + 1) / 2; i++) {
+    for (int i = 0; i < (input.size() + 1) / 2; i++) {
         int n = getInt(',', i, input);
-        resize(&fish, Lanternfish(n), length);
-        length++;
+        fish[n]++;
     }
-    // Brute force solution. Linux + 24 GB of ram = GG
+    // ---- Part 1 ----
     for (int day = 1; day <= 80; day++) {
-        int newFish = 0;
-        for (int i = 0; i < length; i++) {
-            if (fish[i].getTimer() == 0) newFish++;
-            fish[i].passDay();
-        }
-        for (int i = 1; i <= newFish; i++) {
-            resize(&fish, Lanternfish(), length);
-            length++;
-        }
-        cout << "Day " << day << " has just passed." << endl;
+        long i = fish[0];
+        shift(&fish, 9);
+        fish[6] += i;
+        fish[8] = i;
     }
-    cout << "The Population After 80 Days is: " << length << endl;
+    long count = 0;
+    for (int i = 0; i < 9; i++) count += fish[i];
+    cout << "The Population after 80 Days is: " << count << endl;
+    // ---- Part 2 ----
+    // A new fish on day n, will produce a + 1 new fish where 8 + 6(a) <= m, and 8 + 6(a+1) > m
+    for (int day = 81; day <= 256; day++) {
+        long i = fish[0];
+        shift(&fish, 9);
+        fish[6] += i;
+        fish[8] = i;
+    }
+    count = 0;
+    for (int i = 0; i < 9; i++) count += fish[i];
+    cout << "The Population after 256 Days is: " << count << endl;
     return 0;
 }
