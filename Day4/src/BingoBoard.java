48f4844a769bdd85fb76c41141f25362ca097720
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-06 01:16:03 -0600

    -.- It's fixed now. Comments are back. Still more than 50% java though.

--- Day4/src/BingoBoard.java
+++ Day4/src/BingoBoard.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * This class describes a bingo board. Each bingo board has a 5x5 grid and is won whenever someone has gotten a vertical
+ * or horizontal line.
+ *
+ * @author Sugaku
+ */
 public class BingoBoard {
 
+    /**
+     * The actual board of numbers.
+     */
     int[][] board = new int[5][5];
 
+    /**
+     * Creates a new bingo board using the given string array input.
+     *
+     * @param init The initial state of the bingo board in a string form.
+     */
     BingoBoard (String[] init) {
         for (int i = 0; i < 5; i++) {
             Scanner line = new Scanner(init[i]);
@@ -11,10 +25,20 @@ public class BingoBoard {
         }
     }
 
+    /**
+     * Marks all the cells of the board with the given integer.
+     *
+     * @param m The number that if a cell has should be marked.
+     */
     void mark (int m) {
         for (int i = 0; i < 5; i++) for (int j = 0; j < 5; j++) if (board[i][j] == m) board[i][j] = -1;
     }
 
+    /**
+     * Checks if the board has been solved. Returns true if so.
+     *
+     * @return True if and only if the board contains a horizontal or vertical row of marked cells.
+     */
     boolean check () {
         for (int i = 0; i < 5; i++) for (int j = 0; j < 5; j++) {
             if (board[j][i] != -1) break;
@@ -27,6 +51,11 @@ public class BingoBoard {
         return false;
     }
 
+    /**
+     * Sums the unmarked values of the board and returns it.
+     *
+     * @return The value of unmarked cells in the BingoBoard.
+     */
     int score () {
         int count = 0;
         for (int i = 0; i < 5; i++) for (int j = 0; j < 5; j++) if (board[i][j] != -1) count += board[i][j];
