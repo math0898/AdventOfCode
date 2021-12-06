cd3ff14773175e177159850e9641250444ce56e3
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-06 01:33:11 -0600

    Resized I've shrunk quite a bit and made things run part 1 and 2 inline. Should hopefully put C++ > 50%.

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -4,66 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * The relative path, starting from project source, to the input file.
-     */
-    private static final String filename = "Day1/input.txt";
-
-    /**
-     * As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea
-     * floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the
-     * sea floor depth as the sweep looks further and further away from the submarine.
-     *
-     * The first order of business is to figure out how quickly the depth increases, just so you know what you're
-     * dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or
-     * something.
-     *
-     * To do this, count the number of times a depth measurement increases from the previous measurement. (There is no
-     * measurement before the first measurement.)
-     *
-     * @param data The data to use whilst counting.
-     */
-    public static void part1 (ArrayList<Integer> data) {
+    public static void main (String[] args) {
+        Scanner s;
+        try {
+            s = new Scanner(new File("Day1/input.txt")); 
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
+            return;
+        }
+        ArrayList<Integer> data = new ArrayList<>();
+        while (s.hasNextLine()) data.add(Integer.parseInt(s.nextLine()));
         int count = 0;
         for (int i = 1; i < data.size(); i++) if (data.get(i) > data.get(i -1)) count++;
         System.out.println("The depth increased: " + count + " times.");
-    }
-
-    /**
-     * Instead, consider sums of a three-measurement sliding window. Start by comparing the first and second
-     * three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is
-     * 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in
-     * the second window is larger than the sum of the first, so this first comparison increased.
-     *
-     * Your goal now is to count the number of times the sum of measurements in this sliding window increases from the
-     * previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't
-     * enough measurements left to create a new three-measurement sum.
-     *
-     * @param data The data to use whilst counting.
-     */
-    public static void part2 (ArrayList<Integer> data) {
-        int count = 0;
+        count = 0;
         for (int i = 2; i < data.size() - 1; i++) { // This is the center of the second window in the comparison.
             if (data.get(i + 1) > data.get(i - 2)) count++;
         }
         System.out.println("The 3 count window depth increased: " + count + " times.");
-    }
-
-    /**
-     * Main execution point for the program.
-     *
-     * @param args The arguments that would be given to this program through command line.
-     */
-    public static void main (String[] args) {
-        try {
-            Scanner s = new Scanner(new File(filename));
-            ArrayList<Integer> data = new ArrayList<>();
-            while (s.hasNextLine()) data.add(Integer.parseInt(s.nextLine()));
-            part1(data);
-            part2(data);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
-        }
     }
 }
--- Day2/src/Main.java
+++ Day2/src/Main.java
@@ -3,90 +3,31 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * The relative path, starting from project source, to the input file.
-     */
-    private static final String filename = "Day2/input.txt";
-
-    /**
-     * It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
-     *
-     * - `forward X` increases the horizontal position by X units.
-     * - `down X` increases the depth by X units.
-     * - `up X` decreases the depth by X units.
-     *
-     * The submarine seems to already have a planned course (your puzzle input).
-     *
-     * Calculate the horizontal position and depth you would have after following the planned course. What do you get if
-     * you multiply your final horizontal position by your final depth?
-     */
-    public static void part1 () {
-        try {
-            File f = new File(filename);
-            Scanner s = new Scanner(f);
-            int horizontal = 0;
-            int depth = 0;
-            while (s.hasNextLine()) {
-                String input = s.next();
-                switch (input) {
-                    case "forward" -> horizontal += s.nextInt();
-                    case "up" -> depth -= s.nextInt();
-                    case "down"-> depth += s.nextInt();
-                }
-            }
-            System.out.println("Part 1 (Horizontal, Depth): (" + horizontal + ", " + depth + ") => " + horizontal * depth);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
-        }
-    }
-
-    /**
-     * In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at
-     * 0. The commands also mean something entirely different than you first thought:
-     *
-     * - `down X` increases your aim by X units.
-     * - `up X` decreases your aim by X units.
-     * - `forward X` does two things:
-     *      - It increases your horizontal position by X units.
-     *      - It increases your depth by your aim multiplied by X.
-     *
-     * Using this new interpretation of the commands, calculate the horizontal position and depth you would have after
-     * following the planned course. What do you get if you multiply your final horizontal position by your final depth?
-     */
-    public static void part2 () {
-        try {
-            File f = new File(filename);
-            Scanner s = new Scanner(f);
-            int horizontal = 0;
-            int depth = 0;
-            int aim = 0;
-            while (s.hasNextLine()) {
-                String input = s.next();
-                switch (input) {
-                    case "forward" -> {
-                        int t = s.nextInt();
-                        horizontal += t;
-                        depth += t * aim;
-                    }
-                    case "up" -> aim -= s.nextInt();
-                    case "down"-> aim += s.nextInt();
-                }
-            }
-            System.out.println("Part 2 (Horizontal, Depth): (" + horizontal + ", " + depth + ") => " + horizontal * depth);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
-        }
-    }
-
-    /**
-     * The main execution point of the program.
-     *
-     * @param args The arguments given to the executable.
-     */
     public static void main (String[] args) {
-        part1();
-        part2();
+        Scanner s;
+        try {
+            s = new Scanner(new File("Day2/input.txt"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            for (StackTraceElement se : e.getStackTrace()) System.out.println(se.toString());
+            return;
+        }
+        int horizontal = 0;
+        int depth1 = 0;
+        int depth2 = 0;
+        while (s.hasNextLine()) {
+            String input = s.next();
+            switch (input) {
+                case "forward" -> {
+                    int t = s.nextInt();
+                    horizontal += t;
+                    depth2 += t * depth1;
+                }
+                case "up" -> depth1 -= s.nextInt();
+                case "down"-> depth1 += s.nextInt();
+            }
+        }
+        System.out.println("Part 1 (Horizontal, Depth): (" + horizontal + ", " + depth1 + ") => " + horizontal * depth1);
+        System.out.println("Part 2 (Horizontal, Depth): (" + horizontal + ", " + depth2 + ") => " + horizontal * depth2);
     }
 }
