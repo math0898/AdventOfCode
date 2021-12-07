ef2a6dc973791a93dc9104c7e5d56e1a8584a7c8
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-07 12:11:53 -0600

    Day 3 Cleaning
    Cleaned up day 3... really happy with the optimizations made.

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -13,12 +13,27 @@ using namespace std;
  * @param i The int to add into the array.
  * @param n The length of the array.
  */
-void resize (int **array, int i, int n) {
-    int *temp = new int[n + 1];
+template <class T>
+void resize (T** array, T i, int n) {
+    T *temp = new T[n + 1];
     for (int j = 0; j < n; j++) temp[j] = (*array)[j];
     temp[n] = i;
-    delete (*array);
+    delete(*array);
     *array = temp;
+}
+
+/**
+ * Duplicates the given array into the given pointer. This is a deep copy for primatives.
+ * 
+ * @param array The array to copy.
+ * @param dest The array to override.
+ * @param n The length of the array.
+ */
+template <class T>
+void copyArray (T* array, T** dest, int n) {
+    delete(*dest);
+    *dest = new T[n];
+    for (int i = 0; i < n; i++) (*dest)[i] = array[i];
 }
 
 /**
@@ -37,133 +52,71 @@ int convertBinary (string s) {
 }
 
 /**
- * You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the
- * epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate
- *
- * Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in
- * the diagnostic report.
- *
- * The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position
- * is used.
- *
- * @param filename The file to open and read from to get input.
+ * The main execution point of the program.
  */
-void part1 (string filename) {
+int main () {
+    string filename = "input.txt";
     ifstream *file = new ifstream();
     file->open(filename);
-    if (!file->is_open()) return; // Failed to open file.
+    if (!file->is_open()) return -1; // Failed to open file.
     string input;
     *file >> input;
     int *gammaCounts = new int[input.size()]{0};
     int elapsed = 1;
     for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
+    int length = input.size();
+    int* oxygen = new int[1]{0};
+    int size = 1;
+    oxygen[0] = convertBinary(input);
     while (!file->eof()) {
         *file >> input;
         elapsed++;
         for (int i = 0; i < input.size(); i++) if (input[i] == '1') gammaCounts[i]++;
+        resize(&oxygen, convertBinary(input), size);
+        size++;
+    }
+    int* co2 = new int[0];
+    copyArray(oxygen, &co2, size);
+    int co2R = size;
+    int oxygenR = size;
+    for (int i = 1; i <= length; i++) {
+        if (oxygenR != 1 || co2R != 1) {
+            int oxyOnesCount = 0;
+            int coOnesCount = 0;
+            int power = pow(2, length - i); // This is the bit we are counting.
+            for (int j = 0; j < size; j++) {
+                if ((oxygen[j] & power) == power && oxygen[j] != -1) oxyOnesCount++;
+                if ((co2[j] & power) == power && co2[j] != -1) coOnesCount++;
+            }
+            int oxyRequire = 0;
+            int coRequire = 0;
+            if (oxyOnesCount >= (oxygenR / 2.0)) oxyRequire = power;
+            if (coOnesCount < (co2R / 2.0)) coRequire = power;
+            for (int j = 0; j < size; j++) {
+                if ((oxygen[j] & power) != oxyRequire && oxygen[j] != -1) {
+                    oxygen[j] = -1;
+                    oxygenR--;
+                }
+                else if ((co2[j] & power) != coRequire && co2R != 1 && co2[j] != -1) {
+                    co2[j] = -1;
+                    co2R--;
+                }
+            }
+        }
     }
     string temp = "";
     for (int i = 0; i < input.size(); i++) { gammaCounts[i] > (elapsed / 2) ? temp += '1' : temp += '0'; }
     unsigned int gamma = convertBinary(temp);
+    for (int i = 0; i < size; i++) {
+        if (oxygen[i] != -1) {
+            oxygen[0] = oxygen[i];
+        }
+        if (co2[i] != -1) {
+            co2[0] = co2[i];
+        }
+    }
     cout << "The Power Consumption is: " << gamma * (~gamma % (int)pow(2, input.size())) << endl;
-}
-
-/**
- * Part 2 has quite the lengthy definition even for the important information. You can find it in `description.md`.
- *
- * @parm filename The file to open and read from to get input.
- */
-void part2 (string filename) {
-    ifstream *file = new ifstream();
-    file->open(filename);
-    if (!file->is_open()) return; // Failed to open file.
-    string input;
-    *file >> input;
-    int length = input.size();
-    int *list = new int[1]{0};
-    int size = 1;
-    list[0] = convertBinary(input);
-    while (!file->eof()) {
-        *file >> input;
-        resize(&list, convertBinary(input), size);
-        size++;
-    }
-    int remaining = size;
-    for (int i = 1; i <= length; i++) {
-        if (remaining == 1) break;
-        int onesCount = 0;
-        int power = pow(2, length - i); // This is the bit we are counting.
-        for (int j = 0; j < size; j++) {
-            if (list[j] == -1) continue;
-            if ((list[j] & power) == power) onesCount++;
-        }
-        int require = 0;
-        if (onesCount >= (remaining / 2.0)) require = power;
-        for (int j = 0; j < size; j++) {
-            if (list[j] == -1) continue;
-            if ((list[j] & power) != require) {
-                list[j] = -1;
-                remaining--;
-            }
-        }
-        if (remaining < 4) {
-            for (int j = 0; j < size; j++) if (list[j] != -1) cout << list[j] << endl;
-        }
-    }
-    int oxygen = -1;
-    for (int i = 0; i < size; i++) {
-        if (list[i] != -1) {
-            oxygen = list[i];
-            break;
-        }
-    }
-    cout << "Oxygen Generator Rating: " << oxygen << endl;
-    list = new int[1]{0};
-    size = 1;
-    file->clear();
-    file->seekg(0);
-    *file >> input;
-    list[0] = convertBinary(input);
-    while (!file->eof()) {
-        *file >> input;
-        resize(&list, convertBinary(input), size);
-        size++;
-    }
-    remaining = size;
-    for (int i = 1; i <= length; i++) {
-        if (remaining == 1) break;
-        int onesCount = 0;
-        int power = pow(2, length - i); // This is the bit we are counting.
-        for (int j = 0; j < size; j++) {
-            if (list[j] == -1) continue;
-            if ((list[j] & power) == power) onesCount++;
-        }
-        int require = 0;
-        if (onesCount < (remaining / 2.0)) require = power;
-        for (int j = 0; j < size; j++) {
-            if (list[j] == -1) continue;
-            if ((list[j] & power) != require) {
-                list[j] = -1;
-                remaining--;
-            }
-        }
-    }
-    int carbon = 0;
-    for (int i = 0; i < size; i++) {
-        if (list[i] != -1) {
-            carbon = list[i];
-            break;
-        }
-    }
-    cout << "CO2 Scrubber Rating: " << carbon << endl;
-    cout << "Life Support Rating: " << carbon * oxygen << endl;
-}
-
-/**
- * The main execution point of the program.
- */
-int main () {
-    string file = "input.txt";
-    part1(file);
-    part2(file);
+    cout << "Oxygen Generator Rating: " << oxygen[0] << endl;
+    cout << "CO2 Scrubber Rating: " << co2[0] << endl;
+    cout << "Life Support Rating: " << co2[0] * oxygen[0] << endl;
 }
