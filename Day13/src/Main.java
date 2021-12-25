097e8ffec8788664288178c7da36229317ad6ba7
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-24 20:50:48 -0600

    Part 1
    Sorry I redid the thing for part 2. Anyways only running the loop once results in the solution for part 1. Part 2 should be in here I just haven't pressed submit yet.

--- Day13/src/Main.java
+++ Day13/src/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int size = 15;
+    private static final int size = 1400;
 
     public static void main (String[] args) {
-        File f = new File("Day13/example.txt");
+        int xSize = size;
+        int ySize=  size;
+        File f = new File("Day13/input.txt");
         Scanner file;
         try {
             file = new Scanner(f);
@@ -26,20 +28,49 @@ public class Main {
             board[y][x] = '#';
             line = file.nextLine();
         }
-        ArrayList<Integer> horizontalFolds = new ArrayList<>();
-        ArrayList<Integer> verticalFolds = new ArrayList<>();
+        ArrayList<Integer> folds = new ArrayList<>();
         while (file.hasNextLine()) {
             line = file.nextLine().replace("fold along ", "");
-            if (line.contains("y=")) horizontalFolds.add(Integer.parseInt(line.replace("y=", "")));
-            else verticalFolds.add(Integer.parseInt(line.replace("x=", "")));
+            if (line.contains("y=")) folds.add(Integer.parseInt(line.replace("y=", "")));
+            else folds.add(-1 * Integer.parseInt(line.replace("x=", "")));
         }
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) System.out.print(board[i][j]);
+
+        for (Integer hor : folds) {
+            if (hor > 0) {
+                char[][] temp = new char[hor][xSize];
+                int mark = hor;
+                for (int i = 0; i < hor; i++) for (int j = 0; j < xSize; j++) temp[i][j] = board[i][j];
+                for (int i = hor + 1; i < ySize; i++) {
+                    mark--;
+                    for (int j = 0; j < xSize; j++) if (board[i][j] == '#') temp[mark][j] = '#';
+                }
+                ySize = hor;
+                board = temp;
+            } else {
+                hor *= -1;
+                char[][] temp = new char[ySize][hor];
+                int mark = hor;
+                for (int i = 0; i < ySize; i++) for (int j = 0; j < hor; j++) temp[i][j] = board[i][j];
+                for (int i = 0; i < ySize; i++) {
+                    mark = hor;
+                    for (int j = hor + 1; j < xSize; j++) {
+                        mark--;
+                        if (board[i][j] == '#') temp[i][mark] = '#';
+                    }
+                }
+                xSize = hor;
+                board = temp;
+            }
+        }
+
+        int hashes = 0;
+        for (int i = 0; i < ySize; i++) {
+            for (int j = 0; j < xSize; j++) {
+                if (board[i][j] == '#') hashes++;
+                System.out.print(board[i][j]);
+            }
             System.out.print("\n");
         }
-        System.out.println("\nHorizontal folds: ");
-        for (Integer i : horizontalFolds) System.out.println("> " + i);
-        System.out.println("\nVertical folds: ");
-        for (Integer i : verticalFolds) System.out.println("> " + i);
+        System.out.println("Which has " + hashes + " hashes.");
     }
 }
