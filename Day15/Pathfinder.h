74c75cca6ede6083caad40ac123e08da1cb64f4f
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-25 20:26:05 -0600

    Create Pathfinder.h

---
+++ Day15/Pathfinder.h
@@ -0,0 +1,35 @@
+
+
+/**
+ * 
+ * 
+ */
+class Pathfinder {
+
+    private:
+
+        /**
+         * The cost associated with moving from one point to another.
+         */
+        int** difMap;
+
+        /**
+         * The starting x position.
+         */
+        int startX;
+
+        /**
+         * The starting y position.
+         */
+        int startY;
+
+        /**
+         * The target x position.
+         */
+        int endX;
+
+        /**
+         * The target y position.
+         */
+        int endY;
+};
