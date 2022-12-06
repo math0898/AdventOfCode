59b5dd1e72446efa30ea42c4e4170bb1a59a0d86
math0898 <25396616+math0898@users.noreply.github.com>
2022-12-06 13:35:51 -0600

    Day 06

---
+++ 2022/Day 06/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int arr_length = 4; // 4 for pt1, 14 for pt2
+
+bool containsDupe (char* arr) {
+    for (int i = 0; i < arr_length; i++)
+        for (int j = i + 1; j < arr_length; j++)
+            if (arr[i] == arr[j])
+                return true;
+    return false;
+}
+
+char* shift (char* buffer, char insert) {
+    char* temp = new char[arr_length];
+    for (int i = 1; i < arr_length; i++) temp[i - 1] = buffer[i];
+    temp[arr_length - 1] = insert;
+    delete(buffer);
+    return temp;
+}
+
+int main () {
+
+    fstream* file = new fstream("./input.txt");
+
+    char* buffer = new char[arr_length];
+    for (int i = 0; i < arr_length; i++) buffer[i] = file->get();
+    int read = arr_length;
+
+    while (buffer[arr_length - 1] != (char) EOF && containsDupe(buffer)) {
+        read++;
+        buffer = shift(buffer, file->get());
+    }
+
+    cout << "Found: ";
+    for (int i = 0; i < arr_length; i++) cout << buffer[i];
+    cout << " After: " << read << " Characters." << endl;
+
+    return 0;
+}
